474a755d0dec42850c6298bcd09985451a823469
am1rfy <fajzov.amir.gl@gmail.com>
2022-09-23 23:12:08 +0500

    #4 - Сверстал side-bar (в нём пока только список категорий)

--- src/components/LabelsBar.vue
+++ src/components/LabelsBar.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="side-bar">
-
+    <ul class="list-group">
+      <li class="list-group-item" v-on:click="notReady">Label1</li>
+      <li class="list-group-item" v-on:click="notReady">Label2</li>
+      <li class="list-group-item" v-on:click="notReady">Label3</li>
+      <li class="list-group-item" v-on:click="notReady">Label4</li>
+      <li class="list-group-item" v-on:click="notReady">Label5</li>
+    </ul>
   </div>
 </template>
 
@@ -16,5 +22,20 @@ export default {
 </script>
 
 <style scoped>
-
+  .side-bar {
+    padding: 10px;
+    width: 22.5%;
+  }
+  .list-group-item {
+    padding: 25px;
+    border: none;
+    background-color: var(--bs-body-color);
+    color: white;
+    font-weight: bolder;
+    cursor: pointer;
+  }
+  .list-group-item:hover {
+    background-color: var(--bs-gray-800);
+    color: var(--bs-warning);
+  }
 </style>
