0f25487ddb55895063e753da62be8ec3eee95b2c
am1rfy <fajzov.amir.gl@gmail.com>
2022-09-30 16:17:48 +0500

    #8 - Поработал с директивами v-bind, v-on, привязкой классов

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cards-container">
-    <div class="card bg-dark" v-for="card in cards" :key="card.key">
+    <div class="card bg-dark" v-for="card in cards" :key="card.id">
       <h6 class="card-header"> {{card.path}} </h6>
       <div class="card-body">
         <h5 class="card-title">{{card.title}}</h5>
@@ -18,32 +18,36 @@ export default {
     return {
       cards: [
         {
-          key: 1,
+          id: 1,
           path: 'Path',
           title: 'Title',
+          date: '30.09.2022',
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          key: 2,
+          id: 2,
           path: 'Path',
           title: 'Title',
+          date: '28.09.2022',
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         },
         {
-          key: 3,
+          id: 3,
           path: 'Path',
           title: 'Title',
+          date: '27.09.2022',
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
         },
         {
-          key: 4,
+          id: 4,
           path: 'Path',
           title: 'Title',
+          date: '21.08.2022',
           text: 'Lorem ipsum'
-        },
+        }
       ]
     }
-  }
+  },
 }
 </script>
 
--- src/components/LabelsBar.vue
+++ src/components/LabelsBar.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="side-bar">
     <ul class="list-group">
-      <li class="list-group-item" v-on:click="notReady">Label1</li>
-      <li class="list-group-item" v-on:click="notReady">Label2</li>
-      <li class="list-group-item" v-on:click="notReady">Label3</li>
-      <li class="list-group-item" v-on:click="notReady">Label4</li>
-      <li class="list-group-item" v-on:click="notReady">Label5</li>
+      <li class="list-group-item" :class="{selected: label.isSelected}" @click="select" v-for="label in labels" :key="label.id" :id="label.id">{{ label.title }}</li>
     </ul>
   </div>
 </template>
@@ -13,9 +9,22 @@
 <script>
 export default {
   name: 'NavbarComponent',
+  data() {
+    return {
+      labels: [
+        {id: 1, title: 'Label1', isSelected: false},
+        {id: 2, title: 'Label2', isSelected: false},
+        {id: 3, title: 'Label3', isSelected: false},
+        {id: 4, title: 'Label4', isSelected: false},
+        {id: 5, title: 'Label5', isSelected: false},
+      ]
+    }
+  },
   methods: {
-    notReady: () => {
-      alert('not ready')
+    select(e) {
+      this.labels.forEach(label => {
+        label.id == e.target.id ? label.isSelected = true : label.isSelected = false
+      })
     }
   }
 }
@@ -40,6 +49,9 @@ export default {
   }
   .list-group-item:hover {
     background-color: var(--bs-gray-800);
+    /*color: var(--bs-warning);*/
+  }
+  .selected {
     color: var(--bs-warning);
   }
 </style>
