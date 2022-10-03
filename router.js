827204548d3ead25ba8d1dd4a58bd9fa04079931
am1rfy <fajzov.amir.gl@gmail.com>
2022-10-03 22:23:10 +0500

    #12 - Добавил страницу 404

--- router.js
+++ router.js
@@ -4,18 +4,37 @@ const routes = [
     {
         path: '/',
         name: 'Home',
+        meta: {
+            title: 'Notes'
+        },
         component: () => import('@/views/TodoPage')
     },
     {
         path: '/settings/',
         name: 'Settings',
+        meta: {
+            title: 'Settings'
+        },
         component: () => import ('@/views/SettingsPage')
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        meta: {
+            title: 'Not found'
+        },
+        component: () => import('@/views/NotFoundPage')
     }
 ]
 
 const router = createRouter({
     routes,
     history: createWebHistory()
+})
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title
+    next()
 })
 
 export default router
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h6>404</h6>
+  <router-link to="/">
+    Notes
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage"
+}
+</script>
+
+<style scoped>
+  * {
+    display: flex;
+    justify-content: center;
+    color: white;
+  }
+</style>
