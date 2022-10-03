b59b7fca5d086c45e16a219258ffeea1f106ea22
am1rfy <fajzov.amir.gl@gmail.com>
2022-10-03 20:24:20 +0500

    #12 - Описал роутер и вынес всю логику todo страницы из компонента App в TodoPage

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,16 @@
 <template>
-    <HeaderComponent/>
-    <div class="main-container">
-      <LabelsContainer @selectedLabelChanged="selectedLabelChanged"/>
-      <CardsContainer :active-label-info="activeLabelInfo"/>
-    </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HeaderComponent from './components/HeaderComponent.vue'
-import LabelsContainer from './components/LabelsContainer/LabelsContainer.vue'
-import CardsContainer from './components/CardsContainer/CardsContainer.vue'
+import TodoPage from "@/components/TodoPage";
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent,
-    LabelsContainer,
-    CardsContainer
-  },
-  data() {
-    return {
-      activeLabelInfo: {
-        id: Number,
-        title: String,
-        cardsIds: Array
-      },
-    }
-  },
-  methods: {
-    selectedLabelChanged(id, title, cardsIds) {
-      [this.activeLabelInfo.id, this.activeLabelInfo.title, this.activeLabelInfo.cardsIds] = [id, title, cardsIds]
-    }
+    TodoPage
   }
 }
 </script>
 
-<style>
-  .main-container {
-    margin-top: 16px;
-  }
-</style>
+
---
+++ src/components/TodoPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <HeaderComponent/>
+  <div class="main-container">
+    <LabelsContainer @selectedLabelChanged="selectedLabelChanged"/>
+    <CardsContainer :active-label-info="activeLabelInfo"/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './HeaderComponent.vue'
+import LabelsContainer from './LabelsContainer/LabelsContainer.vue'
+import CardsContainer from './CardsContainer/CardsContainer.vue'
+
+export default {
+  name: 'TodoPage',
+  components: {
+    HeaderComponent,
+    LabelsContainer,
+    CardsContainer
+  },
+  data() {
+    return {
+      activeLabelInfo: {
+        id: Number,
+        title: String,
+        cardsIds: Array
+      },
+    }
+  },
+  methods: {
+    selectedLabelChanged(id, title, cardsIds) {
+      [this.activeLabelInfo.id, this.activeLabelInfo.title, this.activeLabelInfo.cardsIds] = [id, title, cardsIds]
+    }
+  }
+}
+</script>
+
+<style>
+.main-container {
+  margin-top: 16px;
+}
+</style>
