8f6d6aed561f46edeb27983e88c717160305c8f9
am1rfy <fajzov.amir.gl@gmail.com>
2022-10-17 21:17:33 +0500

    #18 - Сверстал authForm, UI компонент ссылки и добавил в страницы входа и регистрации; Также импортировал иконки element plus и глобальный файл со стилями

---
+++ src/components/UI/LinkComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link
+      class="custom-link"
+      :to="url"
+  >
+    {{ text }}
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'LinkComponent',
+  props: {
+    url: String,
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+  .custom-link {
+    text-decoration: none;
+    color: var(--el-color-primary)
+  }
+</style>
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,52 +1,106 @@
 <template>
-  <h6>RegisterPage</h6>
-  <LoginForm
-    @click="register"
-    :username="username"
-    :password="password"
-  />
-  <router-link to="/sign-in">
-    Already have an account
-  </router-link>
+
+  <div class="common-layout">
+    <el-container>
+
+      <el-header class="header-container">
+        <div class="flex-container el-flex-row">
+          <el-icon size="30px">
+            <UserFilled/>
+          </el-icon>
+          <p id="title">Sign up</p>
+        </div>
+      </el-header>
+
+      <el-main>
+        <div class="flex-container el-flex-col">
+          <el-alert
+              class="alert"
+              v-if="errorMsg.length !== 0"
+              :title="errorMsg"
+              type="error"
+              show-icon
+              center
+          />
+          <AuthForm
+            @onSubmit="register"
+          />
+          <LinkComponent
+              :url="'/sign-in'"
+              :text="'Already have an account'"
+          />
+        </div>
+      </el-main>
+
+    </el-container>
+  </div>
 </template>
 
 <script>
-import LoginForm from '@/components/LoginForm'
+import AuthForm from '@/components/AuthForm'
+import LinkComponent from '@/components/UI/LinkComponent'
 import { useUserStore } from '@/stores/userStore'
 
 export default {
   name: 'RegisterPage',
   components: {
-    LoginForm
+    AuthForm,
+    LinkComponent
   },
   data() {
     return {
       userStore: useUserStore(),
-
-      // TODO: эти параметры передавать из формы при событии submit
-      username: 'user',
-      password: '12345678'
+      errorMsg: ''
     }
   },
   methods: {
     async register(username, password) {
-
-      // TODO: убрать хардкод
-      [username, password] = [this.username, this.password]
-
       const {isSuccess, msg} = await this.userStore.handleRegister(username, password)
 
       // TODO: убрать
       console.log(isSuccess, msg)
+
+      if (isSuccess)
+        this.$router.push('/')
+      else
+        this.errorMsg = msg
     }
   }
 }
 </script>
 
 <style scoped>
-* {
-  display: flex;
-  justify-content: center;
-  color: white;
-}
+  .flex-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .el-flex-col {
+    flex-direction: column;
+  }
+  .el-flex-row {
+    flex-direction: row;
+  }
+  .header-container {
+    padding: 15vh 0 5vh 0;
+  }
+  #title {
+    font-size: 30px;
+    font-weight: bolder;
+
+    margin-left: 5px;
+  }
+  .alert {
+    margin-bottom: 16px;
+    max-width: 346px;
+    animation: showBlock 0.25s linear forwards;
+  }
+  @keyframes showBlock {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 </style>
